69a171bc864529a05b281cee7c676030afab0c8e
palomafaria <palomacristinafaria@outlook.com>
2021-09-21 20:26:12 -0300

    tela de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,10 @@
 
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './signin.css';
 import logo from '../../assets/logo.png';
 
-function SignIn() {
+function SignUp() {
+  const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -21,16 +21,17 @@ function SignIn() {
         </div>
 
         <form onSubmit={handleSubmit}>
-          <h1>Entrar</h1>
+          <h1>Cadastrar uma conta</h1>
+          <input type="text" placeholder="Seu nome" value={nome} onChange={ (e) => setNome(e.target.value) }/>
           <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value) }/>
           <input type="password" placeholder="*******" value={password} onChange={(e) => setPassword(e.target.value) } />
-          <button type="submit">Acessar</button>
+          <button type="submit">Cadastrar</button>
         </form>  
 
-        <Link to="/register">Criar uma conta</Link>
+        <Link to="/register">Já possui uma conta? Entre aqui</Link>
       </div>
     </div>
   );
 }
 
-export default SignIn;
+export default SignUp;
